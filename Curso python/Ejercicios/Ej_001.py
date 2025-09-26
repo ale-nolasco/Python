af9099c23a7e89beeb73e50d1e5ff4d85d2bb681
Ale_Nolasco <nolascoalejandro.mac@gmai.com>
2025-09-26 00:15:35 -0600

    Primer commint

---
+++ Curso python/Ejercicios/Ej_001.py
@@ -0,0 +1,4 @@
+#Crear un programa que pida al usuario ingresar su nombre y luego imprima un saludo personalizado.
+
+nombre = input("Ingrese su nombre: ")
+print(f"Hola, {nombre}, ¡bienvenido/a!")
---
+++ Curso python/Ejercicios/Ej_002.py
@@ -0,0 +1,5 @@
+#progrma que calcule suma de dos numeros
+
+a = input("Ingrese un dato: ")
+b = a + 3
+print(b)
